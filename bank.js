c4b480a1115414b620d8445e9a6c51c6a29405c1
shaown <smshaown65@gmail.com>
2021-08-15 02:39:16 +0600

    class3 completed

---
+++ bank.js
@@ -0,0 +1,12 @@
+document.getElementById('login-submit').addEventListener('click', function() {
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get your password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+  
+
+    if(userEmail == 'smshaown65@gmail.com' && userPassword == 'secret'){
+       window.location.href = 'https://github.com/smshaown';
+    }
+})
